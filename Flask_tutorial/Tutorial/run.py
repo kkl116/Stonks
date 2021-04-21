bd3396d390d2c60740979cb9f899b19f1ef65e54
kkl116 <47953667+kkl116@users.noreply.github.com>
2021-04-21 10:52:56 +0100

    git ignore try#2

--- Flask_tutorial/Tutorial/run.py
+++
@@ -1,8 +0,0 @@
-
-from flask_blog import app
-# app variable has to be __init__
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-    
--- flask_app/watchlist/utils.py
+++ flask_app/watchlist/utils.py
@@ -1,4 +1,11 @@
 from flask_table import Table, Col
+from stockquotes import Stock
+
+class Watchlist_Stock(Stock):
+    """Inherits Stock class, but just adding some additional stuff so it automatically
+    creates dicts for flask table"""
+    def __init__(self, ticker):
+
 
 class Watchlist_Table(Table):
     classes = ['table', 'table-hover', 'table-sm', 'table-borderless' 'table-collapse']
