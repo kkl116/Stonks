aefde80f43a2293d857534c030950a8afbf6a786
kkl116 <47953667+kkl116@users.noreply.github.com>
2021-11-26 18:43:19 +0800

    sql config

--- flask_app/config.py
+++ flask_app/config.py
@@ -4,6 +4,9 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #need to adapt database uri 
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     #SQLALCHEMY_DATABASE_URI = "postgres://vfwqihmaukuvny:fafc7739f3a409c0818ce50be4aeeb3c3d3c4400765d978899d173ad534b9781@ec2-79-125-30-28.eu-west-1.compute.amazonaws.com:5432/d69g00kk1p8kuk"
