8e58c10be6725e3d8c0ef2213906e1337eb5c9cd
kkl116 <47953667+kkl116@users.noreply.github.com>
2021-11-26 18:22:15 +0800

    requirements

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask_app import create_app, scheduler
 app = create_app()
 
 if __name__ == '__main__':
-    app.debug = True 
+    app.debug = False 
     #start apscheduler
     with app.app_context():
         from flask_app.scheduled import tasks
